2bf9b3e1bbdc5569172d7a6ade027e282a393da9
r_takahama <r.takahama7591@gmail.com>
2015-01-11 20:04:01 +0900

    update dotfiles (from Yokoi)

--- dotfilesSetup.sh
+++ dotfilesSetup.sh
@@ -1,4 +1,8 @@
 ln -s ~/dotfiles/_vimrc ~/.vimrc
+ln -s ~/dotfiles/_zshenv ~/.zshenv
+ln -s ~/dotfiles/_zshenv.darwin ~/.zshenv.darwin
+ln -s ~/dotfiles/_zshenv.linux ~/.zshenv.linux
 ln -s ~/dotfiles/_zshrc ~/.zshrc
+ln -s ~/dotfiles/_zshrc.darwin ~/.zshrc.darwin
 mkdir -p ~/dotfiles/.vim/backup 2> /dev/null
 
