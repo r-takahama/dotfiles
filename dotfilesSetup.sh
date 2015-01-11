dd9d54583ddefc2a15af497afe4b015a2f9020be
r_takahama <r.takahama7591@gmail.com>
2015-01-11 20:05:59 +0900

    add screenrc

--- dotfilesSetup.sh
+++ dotfilesSetup.sh
@@ -4,5 +4,6 @@ ln -s ~/dotfiles/_zshenv.darwin ~/.zshenv.darwin
 ln -s ~/dotfiles/_zshenv.linux ~/.zshenv.linux
 ln -s ~/dotfiles/_zshrc ~/.zshrc
 ln -s ~/dotfiles/_zshrc.darwin ~/.zshrc.darwin
+ln -s ~/dotfiles/_screenrc ~/.screenrc
 mkdir -p ~/dotfiles/.vim/backup 2> /dev/null
 
